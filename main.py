9d056a55bab6646885226490ea87988af338003e
awxk <52842353+awxk@users.noreply.github.com>
2023-01-05 04:47:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Copyright (c) 2023, awxk (https://github.com/awxk)
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
